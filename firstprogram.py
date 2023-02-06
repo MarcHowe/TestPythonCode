f806bab5e64f2b3416109eeea682ebadc06e2f0d
Marc Howe <marcjhowe@gmail.com>
2023-02-06 23:13:07 +0000

    Adding more study files

---
+++ firstprogram.py
@@ -0,0 +1,13 @@
+x, y, z = 10, 7.5, "Three"
+
+print(x)
+print(y)
+print(z)
+
+name1 = "Jeff"
+name2 = "Benzo"
+name3 = name2 = name1 = "Lesley"
+
+print(name1)
+print(name2)
+print(name3)
---
+++ math.py
@@ -0,0 +1,37 @@
+#Order of operations
+#Please Excuse My Dear Aunt Sally
+# - Parentheses
+# - Exponent
+# - Multiplication/Division
+# - Addition/Subtraction
+
+# This equials 8
+add1 = 3+5
+add2 = 5+3
+print(add1)
+print(add2)
+
+# The frst equals 14 but the next equals negative 14, so we can use -14 in our arithmatic
+sub1 = 20-6
+sub2 = 6-20
+print(sub1)
+print(sub2)
+
+# Both equal the same
+multi1 = 7 * 5
+multi2 = 5 * 7
+print(multi1)
+print(multi2)
+
+# This is an exponent, 5 to the power of 3 = 125
+exp1 = 5**3
+print(exp1)
+
+# This is a floor, which provides the rounded number
+floor = 83//25
+print(floor)
+
+#This is a modulo, wheree only the remainder is returned
+mod1 = 8 % 3
+print(mod1)
+
